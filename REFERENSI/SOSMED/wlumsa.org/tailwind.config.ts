bcbc27ede3e69fe35f0010fa78a7217df5011550
Rendaks <105725708+MRendiks@users.noreply.github.com>
2023-09-19 10:20:04 +0700

    add

---
+++ REFERENSI/SOSMED/wlumsa.org/tailwind.config.ts
@@ -0,0 +1,27 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          "color-scheme": "dark",
+          "primary": "#2e046d", // purple
+          "secondary": "#e7ac3b", // yellow
+          "accent": "#6c703e", // green
+          "neutral": "#444444", // gray
+          "base-100": "#ffffff", // white
+          "info": "#C8E1E7",
+          "success": "#DEF29F",
+          "warning": "#F7E589",
+          "error": "#F2B6B5",
+        },
+      },
+    ],
+  },
+} satisfies Config;
